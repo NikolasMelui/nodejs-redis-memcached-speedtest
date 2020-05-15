93cfaaa10eb892a5b54de5b94600dacb736c36a6
nikolasmelui <slak@samaradom.ru>
2020-05-15 23:45:40 +0400

    Remove all of the helpers and tests

--- src/helpers/index.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-const getApplicationHost = (req) => req.headers.host.split(':')[0];
-const getApplicationPort = (req) => req.headers.host.split(':').pop();
-
-module.exports = { getApplicationHost, getApplicationPort };

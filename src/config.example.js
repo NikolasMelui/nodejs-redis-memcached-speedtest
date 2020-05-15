8b92d446d51244ebfa36ad6545574ca552e35669
nikolasmelui <slak@samaradom.ru>
2020-05-15 23:47:43 +0400

    Add necessary fields to the config example file

--- src/config.example.js
+++ src/config.example.js
@@ -1,6 +1,11 @@
 // const config = {
 //   APPLICATION_HOST: '127.0.0.1',
 //   APPLICATION_PORT: 3000,
+//   REDIS_HOST: '127.0.0.1',
+//   REDIS_PORT: 6379,
+//   REDIS_PASSWORD: 'password',
+//   MEMCACHED_HOST: '127.0.0.1',
+//   MEMCACHED_PORT: 11211,
 // };
 
 // module.exports = config;

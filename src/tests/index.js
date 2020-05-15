d8cc30e71c10d65f2d8d318349045d576d8ec502
nikolasmelui <slak@samaradom.ru>
2020-05-15 23:46:54 +0400

    Add new tests to the custom tests library

--- src/tests/index.js
+++ src/tests/index.js
@@ -19,6 +19,34 @@ const tests = {
       process.exit(1);
     }
   },
+  strictEqual: (description, actual, expected) => {
+    console.log(`Description: ${description}`);
+    try {
+      assert.strictEqual(actual, expected);
+      console.log(
+        '\x1b[32m',
+        `Test passed with result - '${actual}'`,
+        '\x1b[0m',
+      );
+    } catch (error) {
+      console.error(error);
+      process.exit(1);
+    }
+  },
+  deepEqual: (description, actual, expected) => {
+    console.log(`Description: ${description}`);
+    try {
+      assert.deepEqual(actual, expected);
+      console.log(
+        '\x1b[32m',
+        `Test passed with result - '${actual}'`,
+        '\x1b[0m',
+      );
+    } catch (error) {
+      console.error(error);
+      process.exit(1);
+    }
+  },
 };
 
 module.exports = tests;
